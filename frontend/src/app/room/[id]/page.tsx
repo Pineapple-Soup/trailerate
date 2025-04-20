b5aef3daec3bb4b7e5540718e74afb1907b7a66a
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-04-20 03:02:31 -0700

    feat: hide name selection once joined

--- frontend/src/app/room/[id]/page.tsx
+++ frontend/src/app/room/[id]/page.tsx
@@ -36,32 +36,33 @@ const Room = () => {
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data);
       console.log("Socket message received:", data);
-      if (data.type === "user_join") {
-        setUsers(data.connected_users);
-        // Message that all a user has joined
-        // setMessages((prevMessages) => [...prevMessages, data.message]);
-      }
+      console.log("Connected users:", data.data.connected_users);
+      setUsers(data.data.connected_users);
+      // Message that all a user has joined
+      // setMessages((prevMessages) => [...prevMessages, data.message]);
     };
   }, [socket]);
 
   return (
     <div className='font-liberation flex flex-col items-center justify-center min-h-screen bg-gradient-to-t to-accent from-black text-white px-4'>
-      <div>
-        <div>Join the Room!</div>
-        <form onSubmit={handleSubmit} className='flex items-center space-x-4'>
-          <input
-            type='text'
-            placeholder='Enter your name'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className='p-2 rounded border text-white'></input>
-          <button
-            type='submit'
-            className='px-4 py-2 rounded bg-accent text-white cursor-pointer'>
-            Join
-          </button>
-        </form>
-      </div>
+      {!users.length && (
+        <div>
+          <div>Join the Room!</div>
+          <form onSubmit={handleSubmit} className='flex items-center space-x-4'>
+            <input
+              type='text'
+              placeholder='Enter your name'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className='p-2 rounded border text-white'></input>
+            <button
+              type='submit'
+              className='px-4 py-2 rounded bg-accent text-white cursor-pointer'>
+              Join
+            </button>
+          </form>
+        </div>
+      )}
 
       <div className='mt-8'>
         <h2 className='text-lg font-bold'>Connected Users:</h2>
