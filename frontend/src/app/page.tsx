fed43342f7b1c6f70bb18af67cbd8053ba96b8a3
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-04-20 00:54:33 -0700

    feat: join errors

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,14 +7,50 @@ import { useState } from "react";
 
 export default function Home() {
   const [roomCode, setRoomCode] = useState("");
+  const [error, setError] = useState("");
+
+  const showError = (message: string) => {
+    setError(message);
+    setTimeout(() => {
+      setError("");
+    }, 2000);
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle room code submission
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/rooms/validate/${roomCode}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          showError("Failed to validate room code");
+          throw new Error("Failed to validate room code");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (!data.exists) {
+          showError("Invalid room code");
+          throw new Error("Invalid room code");
+        }
+        console.log("Room validated:", data);
+        // Handle successful validation (e.g., navigate to the room)
+        window.location.href = `/room/${roomCode}`;
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle validation error (e.g., show an error message)
+      });
   };
 
   return (
     <main className='font-liberation min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-[var(--color-accent)] to-black space-y-6'>
+      {error && (
+        <div className='font-liberation px-4 py-2 bg-accent text-white rounded-md'>
+          {error}
+        </div>
+      )}
+
       <div className='flex items-center space-x-4'>
         <Image
           src='/assets/movie.svg'
