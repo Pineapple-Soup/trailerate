d4842d98477b8ed7a1b2d6f3adab2426474b965c
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-04-20 12:07:41 -0700

    feat: display users ready

--- frontend/src/app/room/[id]/page.tsx
+++ frontend/src/app/room/[id]/page.tsx
@@ -32,38 +32,48 @@ const Room = () => {
       setTimeout(() => {
         document.body.removeChild(modal);
       }, 500);
-    }, 3000);
+    }, 2000);
+  };
+
+  const readyUp = () => {
+    const r = JSON.stringify({
+      type: "ready",
+      player_name: username,
+      room_code: roomCode,
+    });
+    console.log("Ready up message:", r);
+    socket?.send(r);
   };
 
   useEffect(() => {
     const code = window.location.pathname.split("/").pop() || "";
     setRoomCode(code);
-    fetch(
-      `https://${process.env.NEXT_PUBLIC_BACKEND_URL}/rooms/validate/${roomCode}`,
-      {
-        method: "GET",
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-          // router.push("/");
-          throw new Error("Failed to validate room code");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (!data.exists) {
-          router.push("/");
-          throw new Error("Invalid room code");
-        }
-        console.log("Room validated:", data);
-        // Handle successful validation (e.g., navigate to the room)
-        // window.location.href = `/room/${roomCode}`;
-      })
-      .catch((error) => {
-        console.error(error);
-        // Handle validation error (e.g., show an error message)
-      });
+    // fetch(
+    //   `https://${process.env.NEXT_PUBLIC_BACKEND_URL}/rooms/validate/${roomCode}`,
+    //   {
+    //     method: "GET",
+    //   }
+    // )
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       // router.push("/");
+    //       throw new Error("Failed to validate room code");
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     if (!data.exists) {
+    //       router.push("/");
+    //       throw new Error("Invalid room code");
+    //     }
+    //     console.log("Room validated:", data);
+    //     // Handle successful validation (e.g., navigate to the room)
+    //     // window.location.href = `/room/${roomCode}`;
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //     // Handle validation error (e.g., show an error message)
+    //   });
   }, [roomCode, router]);
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -81,15 +91,39 @@ const Room = () => {
       console.log("Socket message sent");
       setSocket(socket);
     };
+
+    socket.onclose = (event) => {
+      console.warn("Socket closed:", event);
+      showMessage("Connection lost. Attempting to reconnect...");
+      setSocket(null);
+    };
+
+    socket.onerror = (error) => {
+      console.error("Socket error:", error);
+      showMessage("A connection error occurred.");
+    };
   };
 
   useEffect(() => {
-    if (!socket) return;
+    if (!socket) {
+      return;
+    }
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data);
       console.log("Socket message received:", data);
-      console.log("Connected users:", data.data.connected_users);
-      setUsers(data.data.connected_users);
+      // console.log("Connected users:", data.data.connected_users);
+      if (data.type === "user_join") {
+        setUsers(data.data.connected_users);
+      }
+      if (data.type === "user_ready") {
+        setUsers((prevUsers) =>
+          prevUsers.map((user) =>
+            user.player_name === data.data.user
+              ? { ...user, status: "ready" }
+              : user
+          )
+        );
+      }
       showMessage(data.data.message);
       // Message that all a user has joined
       // setMessages((prevMessages) => [...prevMessages, data.message]);
@@ -116,16 +150,21 @@ const Room = () => {
           </form>
         </div>
       ) : (
-        <div className='mt-8'>
+        <div className='mt-8 flex flex-col items-center'>
           <h2 className='text-lg font-bold'>Connected Users:</h2>
           <ul className='list-disc list-inside'>
             {users.map((user, index) => (
               <li key={index} className='font-liberation font-bold flex'>
                 {index + 1}. {user.player_name} -{" "}
-                {user.status == "not_ready" ? "WAITING" : "READY"}{" "}
+                {user.status === "ready" ? "READY" : "WAITING"}
               </li>
             ))}
           </ul>
+          <button
+            className='bg-accent px-4 py-2 text-white font-liberation rounded mt-4'
+            onClick={readyUp}>
+            Ready!
+          </button>
         </div>
       )}
     </div>
