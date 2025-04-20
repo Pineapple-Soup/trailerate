9adcd6dd9bbf40cb94e7282cb5a03cff1659d523
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-04-19 17:15:48 -0700

    style: tailwind and eslint formatting

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,20 +1,18 @@
 // app/page.tsx
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="font-liberation min-h-screen flex flex-row items-center justify-center bg-gradient-to-b from-[var(--color-accent)] to-black">
+    <main className='font-liberation min-h-screen flex flex-row items-center justify-center bg-gradient-to-t to-accent'>
       <Image
-        src="/assets/movie.svg"
-        alt="Movie Icon"
+        src='/assets/movie.svg'
+        alt='Movie Icon'
         width={64}
         height={64}
         unoptimized
-        className="mr-4 filter brightness-0 invert"
+        className='mr-4 filter brightness-0 invert'
       />
-      <h1 className="text-white text-[2rem] font-bold">
-        TraileR&apos;ate
-      </h1>
+      <h1 className='text-white text-[2rem] font-bold'>TraileR&apos;ate</h1>
     </main>
   );
 }
