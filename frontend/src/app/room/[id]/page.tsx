ece49bfa88d64d3564f46343e62803bab8971227
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-04-20 01:18:11 -0700

    feat: create room redirection

---
+++ frontend/src/app/room/[id]/page.tsx
@@ -0,0 +1,9 @@
+const Room = () => {
+  return (
+    <div className='font-liberation flex flex-col items-center justify-center min-h-screen bg-gradient-to-t to-accent from-black text-white px-4'>
+      <div>Creating a room...</div>
+    </div>
+  );
+};
+
+export default Room;
--- frontend/src/components/Create.tsx
+++ frontend/src/components/Create.tsx
@@ -1,15 +1,39 @@
-import Link from "next/link";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { ButtonProps } from "@/types/ButtonProps";
 
-const Create = ({ link, label }: ButtonProps) => {
+const Create = ({ label }: ButtonProps) => {
+  const createRoom = () => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/rooms/create`, {
+      method: "POST",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to create room");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.room_id) {
+          // Redirect to the room page
+          window.location.href = `/room/${data.room_id}`;
+        } else {
+          throw new Error("Room code not found in response");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle error (e.g., show an error message)
+        alert("Failed to create room. Please try again.");
+      });
+  };
+
   return (
-    <Link
-      href={link}
-      className='flex font-liberation font-bold space-x-2 items-center justify-center rounded-md px-4 py-2 bg-accent hover:bg-white hover:text-accent'>
+    <button
+      onClick={createRoom}
+      className='flex cursor-pointer font-liberation font-bold space-x-2 items-center justify-center rounded-md px-4 py-2 bg-accent hover:bg-white hover:text-accent'>
       <BsFillPeopleFill size={50} />
       {label}
-    </Link>
+    </button>
   );
 };
 
